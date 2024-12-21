37f260fff9126eead31b4b5db51c6708db05d100
email.aulirarahmi001@gmail.com <aulirarahmi001@gmail.com>
2024-12-21 23:56:09 +0700

    mengubah path

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if (isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Civic Traffic Knowledge-2</title>
     <!-- Menghubungkan dengan file CSS -->
-    <link rel="stylesheet" href="styles2.css">
+    <link rel="stylesheet" href="css/styles2.css">
 </head>
 <body>
     <div class="header">
